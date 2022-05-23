09eed28bb8d7bd975b4c694861d3fff34e1118da
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-05-23 16:10:02 +0000

    move keygen logic to seperate file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-import secrets
-
 # import validators
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from . import models, schemas
-from .database import get_db, engine
+from . import keygen, models, schemas
+from .database import engine, get_db
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -18,9 +17,8 @@ def read_root():
 
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
-    chars = "ABCDFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    key = keygen.create_random_key()
+    secret_key = keygen.create_random_key(length=8)
     db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
     db.add(db_url)
     db.commit()
@@ -29,3 +27,19 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        message = f"URL '{request.url}' doesn't exist."
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)

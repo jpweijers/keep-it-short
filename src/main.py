5655af43cab2e357288e141b224d6c9cdb632ce1
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-05-23 14:32:45 +0000

    post endpoint for creating urls added

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from src import schemas
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return "Welcome to Keep it Short"
+
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    return f"TODO: Create database entry for: {url.target_url}"
